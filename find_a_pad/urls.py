705e1524299c18884d13394b8c71154e46077cfe
Shalini Pyapali <shalu.pyapali@gmail.com>
2017-07-09 01:46:06 -0700

    add new templates for routing.

--- find_a_pad/urls.py
+++ find_a_pad/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^welcome/', include('find_a_pad_app.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^welcome/', include('find_a_pad_app.urls'))
 ]

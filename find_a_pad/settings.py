41ef4d8a3bb276b665072976a5e8ba34540f20b8
Shalini Pyapali <shalu.pyapali@gmail.com>
2017-07-08 17:12:20 -0700

    requirements.txt and data model.

--- find_a_pad/settings.py
+++ find_a_pad/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'find_a_pad_app.apps.FindAPadAppConfig'
 ]
 
 MIDDLEWARE = [
